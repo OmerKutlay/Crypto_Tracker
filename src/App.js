9c1a09fba504d358c445467140b4a248f17db384
Ömer Kutlay <omer.kutlay@outlook.com>
2024-12-19 18:29:32 +0300

    Sidebar app.js'ye import edildi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Exchanges from "./pages/Exchanges";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-
+      <Sidebar/>
       <Routes>
-        <Route path="/" element={ <Dashboard></Dashboard> }/>
-        <Route path="/exchanges" element={ <Exchanges></Exchanges> }/>
+        <Route path="/" element={<Dashboard></Dashboard>} />
+        <Route path="/exchanges" element={<Exchanges></Exchanges>} />
       </Routes>
       <h3>Crypto</h3>
     </div>
