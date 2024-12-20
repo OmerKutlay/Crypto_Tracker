1d37607e3adef118006a56e10662d1ace0556482
Ömer Kutlay <omer.kutlay@outlook.com>
2024-12-20 20:31:36 +0300

    currency list context doyası oluşturuldu

---
+++ src/store/currrencyListContext.js
@@ -0,0 +1,45 @@
+import { useState, createContext } from "react";
+import API from "../API";
+
+const currencyListContext = createContext({});
+
+export const CurrencyListProvider = (props) => {
+    const [currencyList, setCurrencyList] = useState([]);
+    const [exchanges, setExhanges] = useState([]);
+
+    const getCurrencyList = () => {
+        API.get("/assests").then(res => {
+            setCurrencyList(res.data.data);
+        }).catch(error => {
+            setCurrencyList([]);
+            console.log(error);
+        })
+
+    }
+
+    const getExhanges = () => {
+        API.get("/exchanges").then(res => {
+            setExhanges(res.data.data);
+        }).catch(error => {
+            setExhanges([]);
+            console.log(error);
+        })
+    }
+
+
+    const values = {
+        currencyList: currencyList,
+        exchanges: exchanges,
+        setCurrencyList: getCurrencyList,
+        setExhanges: getExhanges
+    }
+
+    return (
+        <currencyListContext.Provider value={values}>
+            {props.children}
+        </currencyListContext.Provider>
+    );
+}
+
+
+export default currencyListContext;
