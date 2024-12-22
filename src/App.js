f6f7721b6ca3674f626382b5b11f07f5f861bce5
Ömer Kutlay <omer.kutlay@outlook.com>
2024-12-22 16:50:11 +0300

    selected currency ctx ve currency list ctx tanımlandı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect,useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Exchanges from "./pages/Exchanges";
 import Sidebar from "./components/Sidebar";
 import "./Style/styles.scss";
+import selectedCurrencyContext from "./store/selectedCurrencyContext";
+import currencyListContext from "./store/currrencyListContext";
 
 function App() {
+  const selectedCurrencyCTX = useContext(selectedCurrencyContext);
+  const currencyListCTX = useContext(currencyListContext);
+
+  useEffect(() => {
+    currencyListCTX.getExchangesetCurrencyList();
+    currencyListCTX.getExchanges();
+    selecetedCurrencyCTX.getHistory("bitcoin");
+  }, []);
+
+
+
+
   return (
     <div className="App">
       <Sidebar/>
