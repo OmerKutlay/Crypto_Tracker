dddbb28dde941a2653ebfe717ae54ca3b489e332
Ömer Kutlay <omer.kutlay@outlook.com>
2024-12-26 12:31:56 +0300

    Exchanges sayfası yapıldı

--- src/components/CurrencyTable.js
+++ src/components/CurrencyTable.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import currencyListContext from "../store/currrencyListContext";
+import currencyListContext from "../store/currencyListContext";
 import selectedCurrencyContext from "../store/selectedCurrencyContext";
 
 function CurrencyTable() {
