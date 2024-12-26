1f2fb44cf253e81392cbfda80760d79053b3927f
Ömer Kutlay <omer.kutlay@outlook.com>
2024-12-26 12:35:53 +0300

    Exchanges sayfası oluşturuldu

--- src/pages/Exchanges.js
+++ src/pages/Exchanges.js
@@ -1,9 +1,60 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { useEffect, useState, useRef, useContext } from 'react';
+import ReactPaginate from 'react-paginate';
+import currencyListContext from '../store/currencyListContext';
+import ExchangesTable from "../components/ExchangesTable";
 
-export default class Exchanges extends Component {
-  render() {
-    return (
-      <h3>Exchanges</h3>
-    )
+const itemsPerPage = 20;
+
+
+const Exchanges = () => {
+  const { exchanges } = useContext(currencyListContext);
+
+
+  const tableRef = useRef();
+
+  const [currentRows, setCurrentRows] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [rowsOffset, setRowsOffset] = useState(0);
+
+  useEffect(() => {
+    const endOffset = rowsOffset + itemsPerPage;
+    setCurrentRows(exchanges.slice(rowsOffset, endOffset));
+    setPageCount(exchanges.length / itemsPerPage);
+  }, [rowsOffset, exchanges]);
+
+  const handlePageClick = (event) => {
+  const newOffset = (event.selected * itemsPerPage) % exchanges.length;
+  setRowsOffset(newOffset);
+  tableRef.current.scrollIntoView();
   }
-}
+  return (
+    <main className='main'>
+      <section ref={tableRef}>
+        <div className='title'>
+          <h4>Exchanges</h4>
+        </div>
+        <ExchangesTable rows={currentRows}></ExchangesTable>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={2}
+          pageCount={pageCount}
+          previousLabel="<"
+          randerOnZeroPageCount={null}
+          containerClassName='pagination'
+          activeClassName='active'
+          pageLinkClassName='page-num'
+          previousClassName='page-num'
+          pageClassName='page-num'
+          previousLinkClassName='page-num'
+          nextClassName='page-num'
+          nextLinkClassName='page-num'
+        />
+      </section>
+    </main>
+  );
+  };
+
+  export default Exchanges;
